48bf010f8248b66c0626ebf464cadb79a371de63
toscy <123191534+Lajous-Harold@users.noreply.github.com>
2025-04-01 14:32:25 +0200

    Add test & Dockerfile

--- src/test/java/com/example/RentACar/RentACarApplicationTests.java
+++ src/test/java/com/example/RentACar/RentACarApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.RentACar;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,10 @@ class RentACarApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Car car = new Car("AA11BB", "ferrari", 2000);
+		String plate = car.getPlateNumber();
+
+		assertEquals("AA11BB", plate);
 	}
 
 }
